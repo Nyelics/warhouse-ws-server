d5dffa9d1b615f3437afa0d79802d3ed86d8b8e0
Nyel <120316605+nyelandres@users.noreply.github.com>
2024-04-12 12:32:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://warehouse-client-gilt.vercel.app/",
-    ],
+    origin: ["https://warehouse-client-gilt.vercel.app"],
     method: ["GET", "POST", "PATCH"],
   },
 });
